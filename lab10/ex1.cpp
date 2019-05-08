23e7853293f0702b6c8af810cc2fd0b0fa9353d2
ikon <emelyanovsergeyofficial@gmail.com>
2019-05-09 00:58:44 +0300

    lab10

--- lab10/ex1.cpp
+++ lab10/ex1.cpp
@@ -11,13 +11,49 @@
 
 #include <conio.h>
 #include <iostream>
+#include <fstream>
 #include <string>
 
 using namespace std;
 
 int main()
 {
-    string *str = new string[64];
+    const int STR_LEN = 64;
+    string *str = new string[STR_LEN];
+    string sentence = "";
+    int len, n = 0;
+
+    ifstream file;
+    file.open("string.txt");
+    if (file.is_open())
+        while (!file.eof() && n < STR_LEN)
+            file >> str[n++];
+    file.close();
+    len = n;
+
+    cout << "All words:" << endl;
+    for (int i = 0; i < len; i++)
+        cout << str[i] << endl;
+
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (str[i].length() > str[j].length()) {
+                string buf = str[i];
+                str[i] = str[j];
+                str[j] = buf;
+            }
+        }
+    }
+
+    cout << "All words after sorting:" << endl;
+    for (int i = 0; i < len; i++)
+        cout << str[i] << endl;
+
+    for (int i = 0; i < len; i++) {
+        if (str[i].length() > 2) {
+
+        }
+    }
 
     delete str;
     getch();
