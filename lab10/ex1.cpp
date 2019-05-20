9085d8a57dbf70e925d7e7a8146eb8cb322c3c9b
ikon <emelyanovsergeyofficial@gmail.com>
2019-05-20 21:39:48 +0300

    lab10.1

--- lab10/ex1.cpp
+++ lab10/ex1.cpp
@@ -20,7 +20,9 @@ int main()
 {
     const int STR_LEN = 64;
     string *str = new string[STR_LEN];
-    int len, n = 0;
+    string sentence = "";
+    string aoe = "aoe";
+    int len, n = 0, kol;
 
     ifstream file;
     file.open("string.txt");
@@ -36,25 +38,63 @@ int main()
 
     for (int i = 0; i < len - 1; i++)
         for (int j = i + 1; j < len; j++)
-            if (str[i].compare(str[j]) > 0)
-                str[i].swap(str[j]);
-
-    for (int i = 0; i < len - 1; i++)
-        for (int j = i + 1; j < len; j++)
-            if (str[i].length() > str[j].length())
+            if (str[i].length() > str[j].length() || (str[i].length() == str[j].length() && str[i].compare(str[j]) > 0))
                 str[i].swap(str[j]);
 
     cout << endl << "All words after sorting:" << endl;
     for (int i = 0; i < len; i++)
         cout << str[i] << endl;
 
-    for (int i = 0; i < len; i++) {
-        if (str[i].length() > 2) {
-
+    for (int i = 0; i < len; i++)
+        if ((kol = str[i].length()) > 2) {
+            for (int j = 0; j < aoe.length(); j++)
+                if (kol > str[i].find(aoe[j]))
+                    kol = str[i].find(aoe[j]);
+            if (kol != str[i].length()) {
+                for (int j = 0; j < kol; j++)
+                    str[i].push_back(str[i][j]);
+                str[i].erase(0, kol);
+            }
+            sentence.append(str[i] + " ");
         }
-    }
+
+    cout << endl << "Sentence:" << endl;
+    cout << sentence << endl;
 
     delete str;
     getch();
     return 0;
 }
+
+
+    /*
+    for (int i = 0; i < len - 1; i++) {
+        int len_i, len_j;
+        for (int j = i + 1; j < len; j++) {
+            len_i = str[i].length();
+            len_j = str[j].length();
+            if (len_i > len_j || (len_i == len_j && str[i].compare(str[j]) > 0))
+                str[i].swap(str[j]);
+        }
+    }
+    */
+
+    /*
+    for (int i = 0; i < len - 1; i++) {
+        int len_i, len_j;
+        for (int j = i + 1; j < len; j++) {
+            len_i = str[i].length();
+            len_j = str[j].length();
+            if (len_i > len_j)
+                str[i].swap(str[j]);
+            else if (len_i == len_j && str[i].compare(str[j]) > 0)
+                str[i].swap(str[j]);
+        }
+    }
+    */
+
+    /*
+    cout << endl << "All words after creating the sentence:" << endl;
+    for (int i = 0; i < len; i++)
+        cout << str[i] << endl;
+    */
